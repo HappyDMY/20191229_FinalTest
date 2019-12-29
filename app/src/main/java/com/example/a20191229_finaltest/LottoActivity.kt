73ea54ce8333797351a940817a54354a1fb6334a
jun <vagrantsj@gmail.com>
2019-12-29 10:29:52 +0900

    로또 화면 초안 작성 오류 있음

---
+++ app/src/main/java/com/example/a20191229_finaltest/LottoActivity.kt
@@ -0,0 +1,19 @@
+package com.example.a20191229_finaltest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LottoActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lotto)
+    }
+
+    override fun setupEvents() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun setValues() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
